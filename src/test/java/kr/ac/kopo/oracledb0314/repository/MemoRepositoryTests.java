4e4a790bcef81392d2c0ae024bfaee0054d0a778
kimprosminju <b.m1nj00371@gmail.com>
2024-03-28 09:45:47 +0900

    0328 수업내용 페이징 처리 2

--- src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
+++ src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
@@ -1,11 +1,17 @@
 package kr.ac.kopo.oracledb0314.repository;
 
+import jakarta.transaction.Transactional;
 import kr.ac.kopo.oracledb0314.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.stream.IntStream;
+import java.util.Optional;
 
 @SpringBootTest
 public class MemoRepositoryTests {
@@ -23,6 +29,65 @@ public class MemoRepositoryTests {
         IntStream.rangeClosed(1, 100).forEach(i ->{
                 Memo memo = Memo.builder().memoText("Dummy Data Test" + i).build();
                 memoRepository.save(memo);
+        });
+    }
+
+    @Test
+    public void testSelect(){
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("=================================");
+
+        if (result.isPresent()){
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+
+    @Transactional
+    @Test
+    public void testSelect2(){
+        Long mno = 100L;
+        Memo memo = memoRepository.getOne(mno);
+        System.out.println("===============================");
+        System.out.println(memo);
+    }
+
+    @Test
+    public void testUpdate(){
+        Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+        System.out.println(memoRepository.save(memo));
+    }
+
+    @Test
+    public void testDelete(){
+        Long mno = 100L;
+        memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault(){
+        Pageable pageable = (Pageable) PageRequest.of(0,10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+        System.out.println("=================================");
+        System.out.println("Total Pages : " + result.getTotalPages());
+        System.out.println("Total Count : " + result.getTotalElements());
+        System.out.println("Page Number : " + result.getNumber());
+        System.out.println("Page Size : " + result.getSize());
+        System.out.println("has next page? : " + result.hasNext());
+        System.out.println("first page? : " + result.isFirst());
+    }
+
+    @Test
+    public void testSort(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo ->{
+            System.out.println("number : " + memo.getMno() + ", content : " + memo.getMemoText());
         });
     }
 }
